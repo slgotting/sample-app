09a2817c70439a7285c1a755b60938b9c0fa2ffb
Steven <sgotting21@yahoo.com>
2019-09-14 20:50:52 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_editable)
+    render 'edit'
+  end
+  
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def user_editable
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resources :users
 
   get '/signup',   to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
